2a33a00de49848a0e650d92b8b4a7e0ac5a36be5
shubham tripathi <117027695+tripathi1996@users.noreply.github.com>
2022-12-18 19:42:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
+import TodoApp from './components/TodoApp';
+
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        AddTodo / >
+        <
+        Todo / >
+        <
+        TodoApp / >
+
+
+
+
+        <
+        /div>
+    );
+}
+
+export default App;
